085b21a805e7460ca9cee4ebe9e8ff4594cf6b78
j0hanz <golfarenlj@gmail.com>
2025-09-15 08:04:00 +0200

    Organize all translation keys

---
+++ src/shared/translations/default.ts
@@ -0,0 +1,11 @@
+export default {
+  errorMissingServerUrl: "Ange FME server-URL.",
+  errorInvalidServerUrl: "Ange en giltig FME server-URL.",
+  errorBadBaseUrl:
+    "Ange en giltig FME server-URL, t.ex. https://fme.server.com.",
+  errorMissingToken: "Ange API-nyckeln.",
+  errorTokenIsInvalid: "API-nyckeln är ogiltig.",
+  errorRepositoryNotFound: "Valt repository finns inte i listan.",
+  errorRepoRequired: "Välj ett repository.",
+  errorInvalidEmail: "Ogiltig e‑postadress.",
+}
